a7dd180d87b7da1dd558f95923b710fd97bf7962
thibault <thibault.daboville@gmail.com>
2017-02-08 11:55:44 +0100

    jerome

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $("#button").click(function(){
+        var task = $("#task").val();
+        console.log("click")
+        $("ul").append("<li><input id='checkbox' type='checkbox'>" + task + "</li>")
+      })
+  })
+
 // alert("Hello World !");
 //
 // var newTask = document.getElementById("taskInput").value;
